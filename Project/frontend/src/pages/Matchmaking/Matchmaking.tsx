28047a75ccc8a98190644cb594f2473ab256f995
UlysseTenret <ulysse.tenret@gmail.com>
2023-10-23 18:13:42 +0200

    wip

--- Project/frontend/src/pages/Matchmaking/Matchmaking.tsx
+++ Project/frontend/src/pages/Matchmaking/Matchmaking.tsx
@@ -1,10 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import io from 'socket.io-client';
 import { useNavigate } from 'react-router-dom';
 import './Matchmaking.css';
 
 function Matchmaking() {
     const navigate = useNavigate();
 
+    useEffect(() => {
+        const serverAddress = window.location.hostname === 'localhost' ?
+            'http://localhost:3002' :
+            `http://${window.location.hostname}:3002`;
+
+        const socketConnection = io(serverAddress, {
+            withCredentials: true
+        });
+
+        socketConnection.on('matchFound', (data: any) => {
+            console.log('Match found!', data);
+            // navigate("/game");  // Navigate to the game when a match is found
+            navigate("/game", { state: { socket: socketConnection } });
+        });
+
+        return () => {
+            socketConnection.close();
+        };
+    }, [navigate]);
 
     return (
         <div className="paddle-container">
