5867c96d84f3b0b17291fbd33984d775eb57284e
victor <victordeleusse@gmail.com>
2023-11-01 19:32:35 +0100

    Working on messages

--- Project/frontend/src/pages/ConversationChannelPage.tsx
+++ Project/frontend/src/pages/ConversationChannelPage.tsx
@@ -1,8 +1,17 @@
 import { useParams } from "react-router-dom";
 import { ConversationChannelPageStyle } from "../utils/styles"
+import { getConversationsIdentified } from "../utils/hooks/getConversationsIdentified";
 
 export const ConversationChannelPage = () => {
-	console.log(useParams())
+	console.log({"USEPARAMS()":useParams()});
+	const id = useParams();
+
+	const fetchConversations = async () => {
+		const conversations = await getConversationsIdentified(id); // Pass the id as an argument
+	};
+	
+	fetchConversations();
+
 	return (
 		<ConversationChannelPageStyle>
 			Channel Page
---
+++ Project/frontend/src/utils/hooks/getConversationsIdentified.tsx
@@ -0,0 +1,15 @@
+import axios, { AxiosRequestConfig } from 'axios';
+
+export async function getConversationsIdentified(id: any) {
+	try {
+	  const API_URL = 'http://localhost:3001/messages';
+	  const config: AxiosRequestConfig = { withCredentials: true };
+  
+	  const response = await axios.get(`${API_URL}/${id}`, config);
+	  console.log({"RESPONSE FROM AXIOS.GET": response.data});
+	  return response.data;
+	} catch (error) {
+	  console.error('Error getting messages from the conversation:', error);
+	  return { messages: [] }; // Return an empty array in case of an error
+	}
+}
