4540e05733b0d8eb36f8e72a76811ad5508ef31c
UlysseTenret <ulysse.tenret@gmail.com>
2023-11-30 16:07:24 +0100

    fixed context

--- Project/frontend/src/SocketContext.tsx
+++ Project/frontend/src/SocketContext.tsx
@@ -25,51 +25,30 @@ export const OnlySocketProvider: React.FC<SocketProviderProps> = ({ children }) 
     const [newMessageReceived, setNewMessageReceived] = useState(false);
     const [isLastMessageDeleted, setLastMessageDeleted] = useState(false);
     const [conversationId, setConversationId] = useState<number | null>(null);
+    const [reinitializeSocket, setReinitializeSocket] = useState(false);
 
     const serverAddress = window.location.hostname === 'localhost' ?
         'http://localhost:3001' :
         `http://${window.location.hostname}:3001`;
 
     useEffect(() => {
-        const socketConnection = io(serverAddress, {
-            withCredentials: true
-        });
+        const socketConnection = io(serverAddress, { withCredentials: true });
         setSocket(socketConnection);
 
         socketConnection.on('disconnect', (reason) => {
-            // Logic to handle reconnection
-            if (reason === "io client disconnect" || reason === "io server disconnect") {
-                // if (reason === "io client disconnect") {
-                console.log('Socket manually disconnected, attempting to reconnect...');
-                // the disconnection was initiated by the server, you need to reconnect manually
-                const newSocketConnection = io(serverAddress, {
-                    withCredentials: true
-                });
-                setSocket(newSocketConnection);
+            console.log("Socket disconnected, reason:", reason);
+            if (reason === "io server disconnect" || reason === "io client disconnect") {
+                console.log('Triggering socket reinitialization...');
+                setReinitializeSocket(prev => !prev); // Toggle to trigger useEffect
+            } else {
+                console.log("Socket disconnected... attempting to reconnect");
             }
-            else
-                console.log("socket disconnected... attempting to reconnect");
         });
 
-        // Cleanup function to disconnect socket when the component unmounts
         return () => {
             socketConnection.disconnect();
         };
-    }, []);
-
-    // const disconnectAndReconnect = (currentSocket?: Socket) => {
-    //     if (currentSocket) {
-    //         console.log({"Socket Disconnected -> Ready To Get A New One": socket?.id});
-    //         currentSocket.disconnect();
-    //     }
-
-    //     const newSocketConnection = io(serverAddress, {
-    //         withCredentials: true
-    //     });
-
-    //     console.log({"New Socket after Deconnection": newSocketConnection?.id});
-    //     setSocket(newSocketConnection);
-    // };
+    }, [reinitializeSocket]); // Depend on reinitializeSocket state
 
 
     return (
