1297f20912ad3c078334468dad808e09a640d8e7
UlysseTenret <ulysse.tenret@gmail.com>
2024-01-30 23:07:57 +0100

    fixed some localhost that were still here

--- Project/frontend/src/pages/Friends/FriendsPage.tsx
+++ Project/frontend/src/pages/Friends/FriendsPage.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { useEffect, useState, useContext } from 'react';
-import { Friendspage, MainContentContainer, InvitationContainer, InvitationsListContainer, InvitationBarContainer, FriendsListContainer, FriendsListTitle, FriendItem, InvitationItem, InvitationBar} from './FriendsElems';
+import { Friendspage, MainContentContainer, InvitationContainer, InvitationsListContainer, InvitationBarContainer, FriendsListContainer, FriendsListTitle, FriendItem, InvitationItem, InvitationBar } from './FriendsElems';
 import { useSocket } from '../../SocketContext';
 import { getFriendsList } from '../../utils/hooks/getFriendsList';
 import { getInvitationsList } from '../../utils/hooks/getInvitationsList';
@@ -23,7 +23,7 @@ export const FriendsPage = () => {
                 console.error('Error fetching friends list:', error);
             }
         };
-        
+
         const fetchInvitationsList = async () => {
             try {
                 const invitationsList = await getInvitationsList();
@@ -33,18 +33,20 @@ export const FriendsPage = () => {
                 console.error('Error fetching invitations list:', error);
             }
         };
-        
+
         fetchFriendsList();
         fetchInvitationsList();
 
         const socket = chatSocketContextData?.socket;
-        if (socket) { 
+        if (socket) {
             socket.on('changeInFriendship', fetchFriendsList);
-            socket.on('changeInFriendship', fetchInvitationsList); }
+            socket.on('changeInFriendship', fetchInvitationsList);
+        }
         return () => {
-            if (socket) { 
+            if (socket) {
                 socket.off('changeInFriendship', fetchFriendsList);
-                socket.off('changeInFriendship', fetchInvitationsList); }
+                socket.off('changeInFriendship', fetchInvitationsList);
+            }
         };
 
     }, [chatSocketContextData]);
@@ -59,7 +61,7 @@ export const FriendsPage = () => {
                 console.error('Error fetching friends list:', error);
             }
         };
-        
+
         const fetchInvitationsList = async () => {
             try {
                 const invitationsList = await getInvitationsList();
@@ -69,7 +71,7 @@ export const FriendsPage = () => {
                 console.error('Error fetching invitations list:', error);
             }
         };
-        
+
         fetchInvitationsList();
         fetchFriendsList();
 
@@ -78,22 +80,24 @@ export const FriendsPage = () => {
         return () => {
             if (socket) { socket.off('changeInFriendship', fetchFriendsList) }
         };
-        
+
     }, [chatSocketContextData]);
 
     const handleRemoveFriend = async (friendId: number) => {
         try {
-          const removed_friend = await axios.post('http://localhost:3001/users/remove_friend', { friendId: friendId }, {
-            withCredentials: true });
+            const removed_friend = await axios.post(`http://${process.env.SERVER_ADRESS}:3001/users/remove_friend`, { friendId: friendId }, {
+                withCredentials: true
+            });
         } catch (error) {
-          console.error('Error removing friend:', error);
+            console.error('Error removing friend:', error);
         }
     };
 
     const handleSendInvitation = async (invitationDetails: { usernameOrEmail: string }) => {
         try {
-            const invitation = await axios.post('http://localhost:3001/users/send_invitation', { userName: invitationDetails.usernameOrEmail }, {
-              withCredentials: true });
+            const invitation = await axios.post(`http://${process.env.SERVER_ADRESS}:3001/users/send_invitation`, { userName: invitationDetails.usernameOrEmail }, {
+                withCredentials: true
+            });
         } catch (error) {
             console.error('Error inviting friend:', error);
         }
@@ -102,22 +106,24 @@ export const FriendsPage = () => {
     const handleAcceptInvitation = async (invitationId: number) => {
         console.log("Invitation from id USER :", invitationId);
         try {
-          const added_friend = await axios.post('http://localhost:3001/users/add_friend', { invitationId: invitationId }, {
-            withCredentials: true });
+            const added_friend = await axios.post(`http://${process.env.SERVER_ADRESS}:3001/users/add_friend`, { invitationId: invitationId }, {
+                withCredentials: true
+            });
         } catch (error) {
-          console.error('Error removing friend:', error);
+            console.error('Error removing friend:', error);
         }
     };
 
     const handleRefuseInvitation = async (invitationId: number) => {
         try {
-          const refused_friend = await axios.post('http://localhost:3001/users/refuse_invitation', { invitationId: invitationId }, {
-            withCredentials: true });
+            const refused_friend = await axios.post(`http://${process.env.SERVER_ADRESS}:3001/users/refuse_invitation`, { invitationId: invitationId }, {
+                withCredentials: true
+            });
         } catch (error) {
-          console.error('Error removing friend:', error);
+            console.error('Error removing friend:', error);
         }
     };
-    
+
 
     return (
         <Friendspage>
@@ -126,11 +132,11 @@ export const FriendsPage = () => {
                     <FriendsListTitle>Friends</FriendsListTitle>
                     <div>
                         {friendsList.map((friend) => (
-                        <FriendItem
-                            key={friend.id}
-                            friend={{ id: friend.id, username: friend.username, status: friend.status }}
-                            removeFriend={handleRemoveFriend}
-                        />
+                            <FriendItem
+                                key={friend.id}
+                                friend={{ id: friend.id, username: friend.username, status: friend.status }}
+                                removeFriend={handleRemoveFriend}
+                            />
                         ))}
                     </div>
                 </FriendsListContainer>
@@ -140,16 +146,16 @@ export const FriendsPage = () => {
                     </InvitationBarContainer>
                     <InvitationsListContainer>
                         <FriendsListTitle>Invitations</FriendsListTitle>
-                            <div>
-                                {invitationsList.map((invitation) => (
+                        <div>
+                            {invitationsList.map((invitation) => (
                                 <InvitationItem
                                     key={invitation.id}
                                     invitation={{ id: invitation.id, username: invitation.username }}
                                     acceptInvitation={handleAcceptInvitation}
                                     refuseInvitation={handleRefuseInvitation}
                                 />
-                                ))}
-                            </div>
+                            ))}
+                        </div>
                     </InvitationsListContainer>
                 </InvitationContainer>
             </MainContentContainer>
--- Project/frontend/src/utils/hooks/getFriendsList.tsx
+++ Project/frontend/src/utils/hooks/getFriendsList.tsx
@@ -1,15 +1,15 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
 export async function getFriendsList() {
-  try {
-    const API_URL = 'http://localhost:3001/users';
-    const config: AxiosRequestConfig = { withCredentials: true };
+    try {
+        const API_URL = `http://${process.env.SERVER_ADRESS}:3001/users`;
+        const config: AxiosRequestConfig = { withCredentials: true };
 
-    const response = await axios.get(`${API_URL}/get_friends`, config);
-    console.log({"RESPONSE FROM AXIOS.GET FRIENDS LIST": response.data});
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching friends list:', error);
-    return { friendsList: [] }; // Return an empty array in case of an error
-  }
+        const response = await axios.get(`${API_URL}/get_friends`, config);
+        console.log({ "RESPONSE FROM AXIOS.GET FRIENDS LIST": response.data });
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching friends list:', error);
+        return { friendsList: [] }; // Return an empty array in case of an error
+    }
 }
