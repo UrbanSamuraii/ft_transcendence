be4494b7ff74767fb1dce6e49ab2bb76d5c0a8b7
UlysseTenret <ulysse.tenret@gmail.com>
2024-02-01 18:10:50 +0100

    fixed issues with powerPongGame

--- Project/frontend/src/pages/Game/PowerPongGame.tsx
+++ Project/frontend/src/pages/Game/PowerPongGame.tsx
@@ -10,32 +10,35 @@ const targetAspectRatio = 1318 / 807;
 const TARGET_WIDTH = 1318;
 const TARGET_HEIGHT = 807;
 
+type Button = {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    text: string;
+    callback: () => void;
+};
+
 interface Power {
     type: string;
     specialAbility: string;
 }
 
-function PowerPongGame({ }) {
+function PowerPongGame() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const [activeKeys, setActiveKeys] = useState<string[]>([]);
     const activeKeysRef = useRef<string[]>(activeKeys);
-    const [isGamePaused, setGamePaused] = useState(false);
     const { socket } = useSocket();
-    const { id: gameId } = useParams<{ id: string }>(); // Specify type for useParams
+    const { id: gameId } = useParams<{ id: string }>();
     const navigate = useNavigate();
-    const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
     const lastSentWasEmptyRef = useRef(true);
     const { user } = useAuth();
+    const username = user?.username || 'Guest'; // Default to 'Guest' if user is null or undefined
     const [gameData, setGameData] = useState(null);
+    const [buttons, setButtons] = useState<Button[]>([]);
     const [powerBarLevel, setPowerBarLevel] = useState(0); // State to track the power bar level
     const [currentPower, setCurrentPower] = useState<Power | null>(null); // State to track the current power
     const [isWaitingForPlayer, setIsWaitingForPlayer] = useState(true);
-
-    const powers: Power[] = [
-        { type: 'Expand', specialAbility: 'Expand' },
-        { type: 'SpeedBoost', specialAbility: 'SpeedBoost' },
-        { type: 'MultiBall', specialAbility: 'MultiBall' },
-    ];
 
     // Update the interval based on active keys
     useEffect(() => {
@@ -43,119 +46,15 @@ function PowerPongGame({ }) {
 
     }, [activeKeys]);
 
-    function goBackToMainMenu() {
+    const goBackToMainMenu = useCallback(() => {
         navigate("/");
-    }
+    }, [navigate]);
 
-    function goBackToSelectMode() {
+    const goBackToSelectMode = useCallback(() => {
         navigate("/select-mode");
-    }
+    }, [navigate]);
 
-    useEffect(() => {
-        if (!socket) return;
-
-        const handleGameDataUpdate = (data: any) => {
-            if (!user)
-                return;
-            // setIsPowerSelectionActive(false);
-            setIsWaitingForPlayer(false);
-            setGameData(data);
-            if (data.leftPlayerInfo && data.leftPlayerInfo.username === user.username) {
-                setPowerBarLevel(data.leftPlayerInfo.powerBarLevel);
-                setCurrentPower(data.leftPlayerInfo.currentPower)
-                console.log(`powerBarLevel: ${powerBarLevel}`);
-            } else if (data.rightPlayerInfo && data.rightPlayerInfo.username === user.username) {
-                setPowerBarLevel(data.rightPlayerInfo.powerBarLevel);
-                setCurrentPower(data.rightPlayerInfo.currentPower)
-                console.log(`powerBarLevel: ${powerBarLevel}`);
-            }
-            drawGame(data);
-            if (data.isGameOver) {
-                clearInterval(intervalId);
-            }
-            console.log(`username :  ${user.username}`)
-            console.log(`data.leftPlayerInfo.username :  ${data.leftPlayerInfo.username}`)
-            console.log(`data.rightPlayerInfo.username :  ${data.rightPlayerInfo.username}`)
-        };
-        // setPowerSelectionTimer(10);
-
-        socket.emit("attemptReconnect", { username: getCookie("username"), gameId });
-
-        socket.on("updateGameData", handleGameDataUpdate);
-        // A single interval is set up and maintained
-        const intervalId = setInterval(() => {
-            const currentKeys = activeKeysRef.current;
-            if (currentKeys.length > 0) {
-                socket.emit('playerActions', currentKeys);
-                lastSentWasEmptyRef.current = false;
-            } else if (!lastSentWasEmptyRef.current) {
-                socket.emit('playerActions', []);
-                lastSentWasEmptyRef.current = true;
-            }
-        }, 1000 / 60);
-
-        return () => {
-            clearInterval(intervalId);
-            socket.off("updateGameData", handleGameDataUpdate);
-            socket.off('initiatePowerSelection');
-            // setIsPowerSelectionActive(false);
-            // setSelectedPower(null);
-        };
-    }, [socket, gameId]);
-
-
-    useEffect(() => {
-        const handleKeyDown = (event: any) => {
-            console.log(`Key down: ${event.key}`);
-
-            // Adding spacebar to the switch case
-            switch (event.key) {
-                case "w":
-                case "s":
-                case "ArrowUp":
-                case "ArrowDown":
-                case " ": // Spacebar
-                    if (!activeKeys.includes(event.key)) {
-                        setActiveKeys(prevKeys => [...prevKeys, event.key]);
-                    }
-                    break;
-                case "p":
-                case "P":
-                    // Existing pause game logic
-                    break;
-                default:
-                    break;
-            }
-        };
-
-        const handleKeyUp = (event: any) => {
-            // Adding spacebar to the switch case
-            switch (event.key) {
-                case "w":
-                case "s":
-                case "ArrowUp":
-                case "ArrowDown":
-                case " ": // Spacebar
-                    setActiveKeys(prevKeys => prevKeys.filter(key => key !== event.key));
-                    break;
-                default:
-                    break;
-            }
-        };
-
-        window.addEventListener('keydown', handleKeyDown);
-        window.addEventListener('keyup', handleKeyUp);
-
-        return () => {
-            window.removeEventListener('keydown', handleKeyDown);
-            window.removeEventListener('keyup', handleKeyUp);
-        };
-    }, [activeKeys, isGamePaused]);
-
-    // const buttons: any = [];
-    const buttonsRef = useRef<any[]>([]);
-
-    const drawButton = (x: number, y: number, width: number, height: number, text: string, callback: () => void) => {
+    const drawButton = useCallback((x: number, y: number, width: number, height: number, text: string, callback: () => void) => {
         console.log(`Drawing button with text: ${text}`);
 
         const canvas = canvasRef.current;
@@ -185,16 +84,10 @@ function PowerPongGame({ }) {
         ctx.fillText(text, x + width / 2, y + height / 2 + fontSize / 4);
 
         // Store callback and button bounds for click detection
-        buttonsRef.current.push({
-            x, y, width, height, callback, text
-        });
-    }
+        return { x, y, width, height, callback, text };
+    }, []);
 
-    function clearButtons() {
-        buttonsRef.current.length = 0;
-    }
-
-    const drawNet = (data: any) => {
+    const drawNet = useCallback((data: any) => {
         const canvas = canvasRef.current;
         if (!canvas) return;
 
@@ -228,9 +121,9 @@ function PowerPongGame({ }) {
             const y = offsetY + i * (pixelSize + gap);
             ctx.fillRect(x, y, pixelSize, pixelSize);
         }
-    }
+    }, []);
 
-    const drawGame = (data: any) => {
+    const drawGame = useCallback((data: any) => {
         if (!canvasRef.current) return;
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
@@ -271,10 +164,10 @@ function PowerPongGame({ }) {
             const pixelHeight = paddle.height * gameHeight / 100;
             ctx.fillRect(pixelX, pixelY, pixelWidth, pixelHeight);
         });
+        setButtons([]);
 
         if (data.isGameOver) {
             // Constants for layout
-            clearButtons();
             const buttonOffsetY = gameHeight * 0.6;
             const winnerNameFontSize = gameHeight * 0.06;
             const textOffsetY = buttonOffsetY - winnerNameFontSize * 2;
@@ -297,27 +190,37 @@ function PowerPongGame({ }) {
 
             // Draw winner message
             ctx.fillText(`Winner: ${data.winnerUsername}`, textX, offsetY + textOffsetY);
-            const localPlayerUsername = user.username;
+            const localPlayerUsername = username;
 
             // Determine if the local player is the winner or loser
             const isLocalPlayerWinner = localPlayerUsername === data.winnerUsername;
             const isLocalPlayerLoser = localPlayerUsername === data.loserUsername;
+            console.log("Is Local Player Winner:", isLocalPlayerWinner);
+            console.log("Is Local Player Loser:", isLocalPlayerLoser);
+            console.log("data.winnerEloChange:", data.winnerEloChange);
+            console.log("data.loserEloChange:", data.loserEloChange);
+
             const localPlayerCurrentElo = isLocalPlayerWinner ? data.winnerCurrentElo : (isLocalPlayerLoser ? data.loserCurrentElo : null);
             const localPlayerEloChange = isLocalPlayerWinner ? data.winnerEloChange : (isLocalPlayerLoser ? data.loserEloChange : 0);
+            console.log("Local Player Current ELO:", localPlayerCurrentElo);
+            console.log("Local Player ELO Change:", localPlayerEloChange);
+
             const newElo = localPlayerCurrentElo + localPlayerEloChange;
+            console.log("New ELO:", newElo);
 
             ctx.font = `${eloFontSize}px 'Press Start 2P', cursive`; // Update font size
-            const eloChangeText = `Your ELO: ${Math.round(newElo)}`;
+            const eloChangeText = `Your ELO: ${newElo}`;
             ctx.fillText(eloChangeText, textX, offsetY + eloTextOffsetY);
 
-            // Draw buttons
-            drawButton(buttonX, offsetY + buttonOffsetY, buttonWidth, buttonHeight, 'MAIN MENU', goBackToMainMenu);
-            drawButton(buttonX, offsetY + buttonOffsetY + buttonHeight * 1.5, buttonWidth, buttonHeight, 'PLAY AGAIN', goBackToSelectMode);
-
-            // Other game over logic
+            const newButtonMenu = drawButton(buttonX, offsetY + buttonOffsetY, buttonWidth, buttonHeight, 'MAIN MENU', goBackToMainMenu);
+            const newButtonPlayAgain = drawButton(buttonX, offsetY + buttonOffsetY + buttonHeight * 1.5, buttonWidth, buttonHeight, 'PLAY AGAIN', goBackToSelectMode);
+            setButtons(currentButtons => [
+                ...currentButtons,
+                ...(newButtonMenu ? [newButtonMenu] : []),
+                ...(newButtonPlayAgain ? [newButtonPlayAgain] : [])
+            ]);
             console.log('Game over detected.');
         }
-
 
         drawNet(data);
 
@@ -329,7 +232,136 @@ function PowerPongGame({ }) {
         ctx.fillText(data.leftScore, offsetX + 0.25 * gameWidth, offsetY + fontSize);
         ctx.fillText(data.rightScore, offsetX + 0.75 * gameWidth, offsetY + fontSize);
 
-    }
+    }, [canvasRef, drawButton, drawNet, username, goBackToMainMenu, goBackToSelectMode]);
+
+    useEffect(() => {
+        if (!socket) return;
+
+        const handleGameDataUpdate = (data: any) => {
+            if (!user)
+                return;
+            setIsWaitingForPlayer(false);
+            setGameData(data);
+            if (data.leftPlayerInfo && data.leftPlayerInfo.username === user.username) {
+                setPowerBarLevel(data.leftPlayerInfo.powerBarLevel);
+                setCurrentPower(data.leftPlayerInfo.currentPower)
+                // console.log(`powerBarLevel: ${powerBarLevel}`);
+            } else if (data.rightPlayerInfo && data.rightPlayerInfo.username === user.username) {
+                setPowerBarLevel(data.rightPlayerInfo.powerBarLevel);
+                setCurrentPower(data.rightPlayerInfo.currentPower)
+                // console.log(`powerBarLevel: ${powerBarLevel}`);
+            }
+            drawGame(data);
+
+            if (data.isGameOver) {
+                clearInterval(intervalId);
+            }
+        };
+
+        socket.emit("attemptReconnect", { username: getCookie("username"), gameId });
+
+        socket.on("updateGameData", handleGameDataUpdate);
+
+        const intervalId = setInterval(() => {
+            const currentKeys = activeKeysRef.current;
+            if (currentKeys.length > 0) {
+                socket.emit('playerActions', currentKeys);
+                lastSentWasEmptyRef.current = false;
+            } else if (!lastSentWasEmptyRef.current) {
+                socket.emit('playerActions', []);
+                lastSentWasEmptyRef.current = true;
+            }
+        }, 1000 / 60);
+
+        return () => {
+            clearInterval(intervalId);
+            socket.off("updateGameData", handleGameDataUpdate);
+        };
+    }, [socket, gameId, drawGame, user]);
+
+    useEffect(() => {
+        const handleKeyDown = (event: any) => {
+
+            console.log(`Key down: ${event.key}`);
+
+            switch (event.key) {
+                case "w":
+                case "s":
+                case "ArrowUp":
+                case "ArrowDown":
+                case " ":
+                    if (!activeKeys.includes(event.key)) {
+                        setActiveKeys(prevKeys => [...prevKeys, event.key]);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            // if (event.key === "w" || event.key === "s" || event.key === "ArrowUp" || event.key === "ArrowDown" || event.key === " ") {
+            //     socket?.emit('playerActions', [...activeKeys, event.key]);
+            //     lastSentWasEmptyRef.current = false;
+            // }
+        };
+
+        const handleKeyUp = (event: any) => {
+
+            console.log(`Key up: ${event.key}`);
+
+            switch (event.key) {
+                case "w":
+                case "s":
+                case "ArrowUp":
+                case "ArrowDown":
+                case " ":
+                    setActiveKeys(prevKeys => prevKeys.filter(key => key !== event.key));
+                    break;
+                default:
+                    break;
+            }
+            // if (event.key === "w" || event.key === "s" || event.key === "ArrowUp" || event.key === "ArrowDown" || event.key === " ") {
+            //     const updatedKeys = activeKeys.filter(key => key !== event.key);
+            //     socket?.emit('playerActions', updatedKeys);
+            //     lastSentWasEmptyRef.current = updatedKeys.length === 0;
+            // }
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+        window.addEventListener('keyup', handleKeyUp);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+            window.removeEventListener('keyup', handleKeyUp);
+        };
+    }, [activeKeys, socket]);
+
+    const handleCanvasClick = useCallback((e: MouseEvent) => {
+        const canvas = canvasRef.current;
+        if (!canvas) return;
+
+        const rect = canvas.getBoundingClientRect();
+        const mouseX = e.clientX - rect.left;
+        const mouseY = e.clientY - rect.top;
+
+        // Check if click is inside any button
+        for (let btn of buttons) {
+            if (mouseX > btn.x && mouseX < btn.x + btn.width && mouseY > btn.y && mouseY < btn.y + btn.height) {
+                console.log(`Clicked on button: ${btn.text}`);
+                btn.callback();
+                break;
+            }
+        }
+    }, [buttons]);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        if (!canvas) return;
+
+        canvas.addEventListener('click', handleCanvasClick);
+
+        return () => {
+            canvas.removeEventListener('click', handleCanvasClick);
+        };
+    }, [handleCanvasClick]);
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -375,34 +407,7 @@ function PowerPongGame({ }) {
         handleResize();
         window.addEventListener('resize', handleResize);
         return () => window.removeEventListener('resize', handleResize);
-    }, [gameData]);
-
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        if (!canvas) return;
-
-        const handleCanvasClick = (e: any) => {
-            const rect = canvas.getBoundingClientRect();
-            const mouseX = e.clientX - rect.left;
-            const mouseY = e.clientY - rect.top;
-
-            for (let btn of buttonsRef.current) {
-                if (
-                    mouseX > btn.x && mouseX < btn.x + btn.width &&
-                    mouseY > btn.y && mouseY < btn.y + btn.height
-                ) {
-                    console.log(`Clicked on button: ${btn.text}`);
-                    btn.callback();
-                    return; // Break after finding the button
-                }
-            }
-        };
-        canvas.addEventListener('click', handleCanvasClick);
-
-        return () => {
-            canvas.removeEventListener('click', handleCanvasClick);
-        };
-    }, [buttonsRef.current]);
+    }, [gameData, drawGame]);
 
     return (
         <div style={{
@@ -436,5 +441,4 @@ function PowerPongGame({ }) {
 
 
 }
-
 export default PowerPongGame;
