283a77c7280834ec3ce969a36ddcb7800c8ba46e
Anna KV <winhaltman@gmail.com>
2024-02-14 19:08:33 +0100

    working on buttons

--- Project/frontend/src/pages/Profile/Profile.tsx
+++ Project/frontend/src/pages/Profile/Profile.tsx
@@ -35,6 +35,10 @@ function Profile() {
         }
     }, [username]);
 
+    const changeNickname = () => {
+
+    };
+
     const toggleTheme = () => {
         setTheme(prevTheme => {
             switch (prevTheme) {
@@ -150,15 +154,23 @@ function Profile() {
                     <div className={`user-card ${theme}`}>
                         <div className="level">{getEloRank(+userInfo.eloRating)}</div>
                         <div className='profile-picture'>
-                            <img src={userInfo.img_url || "https://openseauserdata.com/files/b261626a159edf64a8a92aa7306053b8.png"} className="rounded-image" width="135" height="135" alt="User avatar" />
-                            <input type="file" ref={fileInputRef} style={{ display: 'none' }} accept=".png" onChange={handleAvatarChange} />
-                            {user && user.username === username && (
-                                <button className='change-avatar' onClick={() => fileInputRef.current?.click()}>Change Avatar</button>
-                            )}</div>
+                        <img 
+                            src={userInfo.img_url || "https://openseauserdata.com/files/b261626a159edf64a8a92aa7306053b8.png"} 
+                            className="rounded-image" width="135" height="135" alt="User avatar" 
+                            onClick={user && user.username === username ? (() => fileInputRef.current?.click()) : undefined}
+                            style={{ cursor: user && user.username === username ? 'pointer' : 'default' }}/>
+                        <input 
+                            type="file" ref={fileInputRef} style={{ display: 'none' }} 
+                            accept=".png" onChange={handleAvatarChange} />
+                        </div>
                         <div className="points">{userInfo.eloRating}</div>
                     </div>
                     <div className="more-info">
                         <h1>{userInfo.username}</h1>
+                        {user && user.username === username && (
+                            <button className="edit-profile-name" onClick={changeNickname}>
+                                <i className='bx bxs-pencil'></i></button>
+                        )}
                         <Link to={`/signout`} className="signout-button">
                             <i className='bx bx-exit'></i></Link>
                         <div className='separator'></div>
@@ -195,8 +207,14 @@ function Profile() {
                         <Link to={`/@/${username}/match-history`} className="leaderboard-button">
                             Match History</Link>
                     </div>
-                    <Link to={`/2fa-enable`} className="twoFA-button">
-                        2FA</Link>
+                    {user && user.username === username && (
+                        <div>
+                        <Link to={`/2fa-enable`} className="twoFA-button-on">
+                        2FA on</Link>
+                        <Link to={`/2fa-disable`} className="twoFA-button-off">
+                        2FA off</Link>
+                        </div>
+                    )}
                     {user && user.username === username && (
                         <button className="edit-profile" onClick={toggleTheme}>
                             <i className='bx bxs-palette'></i></button>
